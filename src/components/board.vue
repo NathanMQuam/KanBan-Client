7230c3cf3222bd5d1d9ede318666da8f61b617cd
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-03-16 15:42:17 -0600

    Created component files

---
+++ src/components/board.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="board">
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+export default {
+  name: 'Board',
+  setup() {
+    const state = reactive({
+      name: '',
+      description: '',
+      creatorId: ''
+    })
+    return {
+      state
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="List">
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+export default {
+  name: 'List',
+  setup() {
+    const state = reactive({
+      name: '',
+      description: '',
+      creatorId: '',
+      boardId: ''
+    })
+    return {
+      state
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
